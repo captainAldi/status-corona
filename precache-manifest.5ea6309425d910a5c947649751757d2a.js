51f58e424ce96be46f0e4b25a3bbdfc25881910e
captainAldi <renaldiyulvianda@yahoo.com>
2020-05-04 19:08:18 +0700

    deploy

---
+++ precache-manifest.5ea6309425d910a5c947649751757d2a.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "798e0b7f4c72bb7b9bbf",
+    "url": "/status-corona/css/chunk-vendors.c1b1ae09.css"
+  },
+  {
+    "revision": "4e895e101c7f5c49f9fa0202a5215056",
+    "url": "/status-corona/img/crying.4e895e10.png"
+  },
+  {
+    "revision": "a525bd7b8f579e0f2b6c9b13bfef00d2",
+    "url": "/status-corona/img/smile.a525bd7b.png"
+  },
+  {
+    "revision": "6e0ac0d5e8b61274199e225a229209fa",
+    "url": "/status-corona/img/smileys.6e0ac0d5.png"
+  },
+  {
+    "revision": "221497a1704085c4d927df025a3ad156",
+    "url": "/status-corona/index.html"
+  },
+  {
+    "revision": "6056645ca60a33bd29d7",
+    "url": "/status-corona/js/about.19bc459d.js"
+  },
+  {
+    "revision": "8e6e247263b608cd0bcf",
+    "url": "/status-corona/js/app.a7551df6.js"
+  },
+  {
+    "revision": "798e0b7f4c72bb7b9bbf",
+    "url": "/status-corona/js/chunk-vendors.73b77268.js"
+  },
+  {
+    "revision": "49d8c04dc7af2fda9c8c3732bd8a2d17",
+    "url": "/status-corona/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/status-corona/robots.txt"
+  }
+]);
